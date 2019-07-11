6b864029d9189c039029dd470e533193ea9033ef
aksowmyasri <ak.sowmya4@gmail.com>
2019-07-11 22:46:05 +0530

    TASK 4

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,13 +1,27 @@
 package com.stackroute;
 import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import java.security.cert.X509Certificate;
+
 public class Main {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("file:src/beans.xml");
-        Movie movie = applicationContext.getBean("movie",Movie.class);
-        movie.displayActor();
-         Movie movie2 = applicationContext.getBean("movie2",Movie.class);
-        movie2.displayActor();
+       Movie movie = applicationContext.getBean("movie",Movie.class);
+       ApplicationContext applicationContext1 = movie.getContext();
+       Movie movie1 = applicationContext1.getBean("movie",Movie.class);
+        System.out.println("Through Application Context");
+       movie1.displayActor();
+        BeanFactory beanFactory = new FileSystemXmlApplicationContext("src/beans.xml");
+        Movie movie2 = beanFactory.getBean("movie",Movie.class);
+        BeanFactory beanFactory1 = movie2.getBeanFactory();
+        Movie movie3 = beanFactory1.getBean("movie",Movie.class);
+        System.out.println("Through Bean Factory");
+        movie3.displayActor();
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,11 +1,19 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 import java.rmi.MarshalledObject;
 
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
 
     private Actor actor;
-
+    private ApplicationContext context = null;
+    private BeanFactory beanFactory = null;
     public Movie(){}
     public Movie(Actor actor) {
         this.actor = actor;
@@ -20,5 +28,29 @@ public class Movie {
         actor.display();
     }
 
+    public BeanFactory getBeanFactory()
+    {
+        return beanFactory;
+    }
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException
+    {
+        this.beanFactory = beanFactory;
+    }
 
+    @Override
+    public void setBeanName(String s)
+    {
+        System.out.println("Bean Name is "+s);
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context = applicationContext;
+
+    }
+    public ApplicationContext getContext() {
+        return context;
+    }
 }
